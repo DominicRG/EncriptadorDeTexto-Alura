33595f48ffdf398476b710aec1c12c8c3f6f49d1
Dominic Roman <romandominic30@gmail.com>
2023-05-13 16:51:27 -0500

    Agregando index.js y elementos HTML al JS

---
+++ index.js
@@ -0,0 +1,9 @@
+//Elementos HTML
+const cajaTexto = document.querySelector('.caja-texto');
+const textoResultado = document.querySelector('.text-resultado');
+const mensajeNoEncontrado = document.querySelector('.msj-no-encontrado');
+const botonEncriptar = document.querySelector('.boton-encriptar');
+const botonDesencriptar = document.querySelector('.boton-desencriptar');
+const botonCopiar = document.querySelector('.boton-copiar');
+const contenedorImagenMuñeco = document.querySelector('.container-imagen-muñeco');
+const contenedorParrafo = document.querySelector('.container-parrafo');
