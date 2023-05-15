67af1f1f00bacc93c391d606afe765b255bcfc20
Dominic Roman <romandominic30@gmail.com>
2023-05-15 12:09:07 -0500

    Modificando index.js(funciones Encrip, DesEncrip, CopyEncrip, Minus&Acentos)

--- index.js
+++ index.js
@@ -7,3 +7,86 @@ const botonDesencriptar = document.querySelector('.boton-desencriptar');
 const botonCopiar = document.querySelector('.boton-copiar');
 const contenedorImagenMuñeco = document.querySelector('.container-imagen-muñeco');
 const contenedorParrafo = document.querySelector('.container-parrafo');
+const contenedorParrafoResultado = document.querySelector('.container-parrafo-resultado');
+const contenedorCopiar = document.querySelector('.container-copiar');
+
+//Funcion Encriptar
+function encriptar(){
+    var texto = cajaTexto.value;
+    if(texto.trim() == ''){
+        alert('Caja vacia');
+    }else{
+        //Ocultando-VistaSinResultado
+        contenedorImagenMuñeco.classList.add('ocultar');
+        mensajeNoEncontrado.classList.add('ocultar');
+        contenedorParrafo.classList.add('ocultar');
+        //Mostrando-CamposDeResultado
+        contenedorParrafoResultado.classList.remove('ocultar');
+        contenedorCopiar.classList.remove('ocultar');
+        
+        var textoEncriptado = '';
+        let obj_letters = {
+            a: 'ai',
+            e: 'enter',
+            i: 'imes',
+            o: 'ober',
+            u: 'ufat',
+        };
+
+        textoEncriptado = texto.replace( /a|e|i|o|u/g, function(matched){return obj_letters[matched]});
+        console.log(textoEncriptado);
+        textoResultado.textContent = textoEncriptado;
+    }
+}
+
+//Funcion Desencriptar
+function desencriptar(){
+    var texto = cajaTexto.value;
+    if(texto.trim() == ''){
+        alert('Caja vacia');
+    }else{
+        //Ocultando-VistaSinResultado
+        contenedorImagenMuñeco.classList.add('ocultar');
+        mensajeNoEncontrado.classList.add('ocultar');
+        contenedorParrafo.classList.add('ocultar');
+        //Mostrando-CamposDeResultado
+        contenedorParrafoResultado.classList.remove('ocultar');
+        contenedorCopiar.classList.remove('ocultar');
+        
+        var textoDesEncriptado = '';
+        let obj_letters = {
+            ai: 'a',
+            enter: 'e',
+            imes: 'i',
+            ober: 'o',
+            ufat: 'u',
+        };
+
+        textoDesEncriptado = texto.replace( /ai|enter|imes|ober|ufat/g, function(matched){return obj_letters[matched]});
+        console.log(textoDesEncriptado);
+        textoResultado.textContent = textoDesEncriptado;
+    }
+}
+//Funcion CopiarEncriptado
+function copiarEncriptado(){
+    let textCopy = textoResultado.textContent;
+    if(textCopy.trim() == ''){
+        alert('Caja vacia');
+    }else{
+        navigator.clipboard.writeText(textCopy);
+        cajaTexto.select();
+        cajaTexto.focus();
+    }
+}
+//Detectar solo introduzca minusculas y sin acentos
+cajaTexto.addEventListener('input', function(){
+    let textInput = cajaTexto.value;
+    //Convertir a minuscula y restringir uso de caracteres especiales y tildes(acentos)
+    textInput = textInput.toLowerCase().replace(/[^a-zñ\s]/g, '');
+    cajaTexto.value = textInput;
+})
+
+//Añadir funciones a los botones
+botonEncriptar.addEventListener('click', encriptar);
+botonDesencriptar.addEventListener('click', desencriptar);
+botonCopiar.addEventListener('click', copiarEncriptado);
