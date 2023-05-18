fa4fb854291d6851208779cb4ae513a8505d3006
Dominic Roman <romandominic30@gmail.com>
2023-05-18 17:17:22 -0500

    DarkMode

--- index.js
+++ index.js
@@ -137,3 +137,18 @@ document.addEventListener('DOMContentLoaded', ocultarCamposDeResultado);
 botonEncriptar.addEventListener('click', encriptar);
 botonDesencriptar.addEventListener('click', desencriptar);
 botonCopiar.addEventListener('click', copiarEncriptado);
+
+
+//DarkMode
+const body = document.querySelector("body");
+const toggle = document.querySelector("#toggle");
+const sunIcon = document.querySelector(".toggle .bxs-sun");
+const moonIcon = document.querySelector(".toggle .bx-moon");
+
+toggle.addEventListener("change", () => {
+    
+    body.classList.toggle("dark");
+    sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+    moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+
+});
