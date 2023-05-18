926a32772f693ab9fa118ee427887ef7909e963b
Dominic Roman <romandominic30@gmail.com>
2023-05-17 20:57:18 -0500

    Animacion y Alert para CajaVacia, funciones para mostrar u ocultar campos

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ const contenedorImagenMuñeco = document.querySelector('.container-imagen-muñec
 const contenedorParrafo = document.querySelector('.container-parrafo');
 const contenedorParrafoResultado = document.querySelector('.container-parrafo-resultado');
 const contenedorCopiar = document.querySelector('.container-copiar');
+const contenedorSection2 = document.querySelector('.container-section2');
 
 //Funcion Encriptar
 function encriptar(){
     var texto = cajaTexto.value;
     if(texto.trim() == ''){
-        alert('Caja vacia');
+        if(contenedorImagenMuñeco.classList.contains('ocultar') && mensajeNoEncontrado.classList.contains('ocultar') && contenedorParrafo.classList.contains('ocultar')){
+            ocultarCamposDeResultado();
+            mostrarVistasSinResultado();
+            agregarAlert();
+        }else{
+            agregarAlert();
+        }
     }else{
         //Ocultando-VistaSinResultado
-        contenedorImagenMuñeco.classList.add('ocultar');
-        mensajeNoEncontrado.classList.add('ocultar');
-        contenedorParrafo.classList.add('ocultar');
+        ocultarVistasSinResultado();
         //Mostrando-CamposDeResultado
-        contenedorParrafoResultado.classList.remove('ocultar');
-        contenedorCopiar.classList.remove('ocultar');
-        botonCopiar.classList.remove('ocultar');
+        mostrarCamposDeResultado();
         
         var textoEncriptado = '';
         let obj_letters = {
@@ -44,16 +47,18 @@ function encriptar(){
 function desencriptar(){
     var texto = cajaTexto.value;
     if(texto.trim() == ''){
-        alert('Caja vacia');
+        if(contenedorImagenMuñeco.classList.contains('ocultar') && mensajeNoEncontrado.classList.contains('ocultar') && contenedorParrafo.classList.contains('ocultar')){
+            ocultarCamposDeResultado();
+            mostrarVistasSinResultado();
+            agregarAlert();
+        }else{
+            agregarAlert();
+        }
     }else{
         //Ocultando-VistaSinResultado
-        contenedorImagenMuñeco.classList.add('ocultar');
-        mensajeNoEncontrado.classList.add('ocultar');
-        contenedorParrafo.classList.add('ocultar');
+        ocultarVistasSinResultado();
         //Mostrando-CamposDeResultado
-        contenedorParrafoResultado.classList.remove('ocultar');
-        contenedorCopiar.classList.remove('ocultar');
-        botonCopiar.classList.remove('ocultar');
+        mostrarCamposDeResultado();
         
         var textoDesEncriptado = '';
         let obj_letters = {
@@ -88,16 +93,45 @@ cajaTexto.addEventListener('input', function(){
     cajaTexto.value = textInput;
 })
 
-//Ocultar elementos iniciles del section2
-function ocultarIni(){
+/*Funciones para mostrar u ocultar campos del section2 */
+function mostrarCamposDeResultado(){
+    contenedorParrafoResultado.classList.remove('ocultar');
+    contenedorCopiar.classList.remove('ocultar');
+    botonCopiar.classList.remove('ocultar');
+}
+
+function ocultarCamposDeResultado(){
     contenedorParrafoResultado.classList.add('ocultar');
     contenedorCopiar.classList.add('ocultar');
     botonCopiar.classList.add('ocultar');
 }
 
+function mostrarVistasSinResultado(){
+    contenedorImagenMuñeco.classList.remove('ocultar');
+    mensajeNoEncontrado.classList.remove('ocultar');
+    contenedorParrafo.classList.remove('ocultar');
+}
+
+function ocultarVistasSinResultado(){
+    contenedorImagenMuñeco.classList.add('ocultar');
+    mensajeNoEncontrado.classList.add('ocultar');
+    contenedorParrafo.classList.add('ocultar');
+}
+
+//Quitar alert luego de la advertencia
+function quitarAlert(){
+    contenedorSection2.classList.remove('alert');
+}
+
+//Animar mensaje de texto no encontrado si la caja esta vacía
+function agregarAlert(){
+    contenedorSection2.classList.add('alert');
+    setTimeout(quitarAlert, 1000);
+}
+
 //Aplicar el ocultar al cargar la pagina
 //window.addEventListener('load', ocultarIni);
-document.addEventListener('DOMContentLoaded', ocultarIni);
+document.addEventListener('DOMContentLoaded', ocultarCamposDeResultado);
 
 //Añadir funciones a los botones
 botonEncriptar.addEventListener('click', encriptar);
